c6824446fd9296b5b89181d1d841c325dae5671c
Jesus <perez.jesus1039@gmail.com>
2023-03-28 19:36:56 -0700

    created the belongsToMany tags for the tag and for the product

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -17,8 +17,16 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: 'prodcut_tag',
+  foreignKey: 'tag_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'product_tag',
+  foreignKey: 'tag_id',
+})
 
 module.exports = {
   Product,
