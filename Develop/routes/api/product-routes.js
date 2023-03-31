7e663a38926c4c565072893b38e752b3e3af1363
Jesus <perez.jesus1039@gmail.com>
2023-03-30 17:16:25 -0700

    created the delete code for deleting one product by the id

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
       include: [{ model: Category, attributes: ['id', 'category_name']}, { model: Tag, 
         attributes: ['id', 'tag_name']}],
     });
-    res.status(200).json(prodcutData);
+    res.status(200).json(productData);
   } catch(err) {
     console.error(err);
     res.status(500).json(err);
@@ -109,8 +109,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(err);
+  } catch(err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
